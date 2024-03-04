1d178100960cb085513e6c36663f3204095f7395
YuyunTs <yuyu.ee12@nycu.edu.tw>
2024-03-04 19:32:42 +0800

    add lab3.py

---
+++ lab3.py
@@ -0,0 +1,8 @@
+# Create a point at the origin
+origin = Point() # this will appear automatically
+
+# Create a point at 3, 4
+point = Point()   # keep press tab and enter
+point.x = 3.0     # keep press tab and enter
+point.y = 4.0     # keep press tab and enter
+
